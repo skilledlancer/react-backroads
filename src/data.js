03972eec2c723611854b6f6b44e97572ed6a7d34
Manish Kumar Namdeo <manish_kumar.namdeo_ext@novartis.com>
2023-01-28 15:35:37 +0530

    NavBar

---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const pageLinks = [
+    {id:1, href:'#home', text:'home'},
+    {id:1, href:'#about', text:'about'},
+    {id:1, href:'#services', text:'services'},
+    {id:1, href:'#tours', text:'tours'},
+]
